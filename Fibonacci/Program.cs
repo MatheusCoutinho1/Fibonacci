d81a9abb514f46930de9c8d41dd95faee8a878d9
Matheus <matheuscnc4@gmail.com>
2023-04-21 02:37:18 -0300

    Segunda questão do teste

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Informe o número a ser verificado na sequência de Fibonacci
+        int numero = 21;
+
+        int a = 0, b = 1, c = 0;
+        bool pertence = false;
+
+        while (c <= numero)
+        {
+            if (c == numero)
+            {
+                pertence = true;
+                break;
+            }
+
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        if (pertence)
+        {
+            Console.WriteLine("O número " + numero + " pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine("O número " + numero + " não pertence à sequência de Fibonacci.");
+        }
+    }
+}
